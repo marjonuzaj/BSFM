598f3c0c3107bef2197528048d4593c78424accb
MariolJonuzaj <marjonuzaj@gmail.com>
2026-05-01 16:32:12 +0100

    main.py runs example

---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import pandas as pd
+from models import SFM
+import matplotlib.pyplot as plt
+
+N = 1000
+x1 = np.random.normal(0,1,(N,1))
+x2 = np.random.normal(0,1,(N,1))
+u = np.abs(np.random.normal(0,0.2,(N,1)))
+v = np.random.normal(0,0.1,(N,1))
+y = 1 + 0.5 *x1 + 0.4 * x2 + v -u
+    
+df = pd.DataFrame()
+df['y'] = y.flatten()
+df['x1'] = x1.flatten()
+df['x2'] = x2.flatten()
+
+model = SFM(df,'y')
+    
+model.fit_half_normal(nsim = 10000, drop= 2000)
+print(model.summary())
+
+plt.scatter(model.inef_est, u)
+plt.xlabel('Inef Est')
+plt.ylabel('Inef Real')
+plt.show()
+
+plt.hist(model.inef_est, bins=50, edgecolor='lightgrey')
+plt.xlabel('Inef Est')
+plt.xlim(0,max(model.inef_est)+0.1)
+plt.show()
